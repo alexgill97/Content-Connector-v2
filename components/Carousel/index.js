e59a0eb5a966d90b17b7adb2aaf69428bab568d6
Alex <alex.gillespie@live.com>
2022-06-26 13:37:45 -0400

    create new image slider

---
+++ components/Carousel/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import BtnSlider from './BtnSlider';
+
+const Slider = ({ images }) => {
+  //Tracks current selected slide
+  const [slideIndex, setSlideIndex] = useState(1);
+
+  const nextSlide = () => {
+    if (slideIndex !== images.length) {
+      setSlideIndex(slideIndex + 1);
+    } else if (slideIndex === images.length) {
+      setSlideIndex(1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1);
+    } else if (slideIndex === 1) {
+      setSlideIndex(images.length);
+    }
+  };
+
+  const moveDot = (index) => {
+    setSlideIndex(index);
+  };
+
+  return (
+    <div className="container-slider">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={slideIndex === index + 1 ? 'slide active-anim' : 'slide'}
+        >
+          <img
+            className="h-full w-full object-cover lg:object-cover"
+            src={image}
+          />
+        </div>
+      ))}
+      <BtnSlider moveSlide={nextSlide} direction={'next'} />
+      <BtnSlider moveSlide={prevSlide} direction={'prev'} />
+
+      <div className="container-dots">
+        {Array.from({ length: images.length }).map((item, index) => (
+          <div
+            onClick={() => moveDot(index + 1)}
+            className={slideIndex === index + 1 ? 'dot active' : 'dot'}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;

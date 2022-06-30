3454a3d9a9e7d1fb99ce12bb0ec7a4404437533c
Alex <alex.gillespie@live.com>
2022-06-30 09:20:34 -0400

    home page initial layout

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,24 @@ export default function Home() {
 
   return (
     <main className={styles.home_main}>
-      <div className={styles.title}>
-        <h2>
+      <div className={styles.heading_main}>
+        <h2 className={styles.copy_main}>
           Connecting Businesses <br />
           with
-          <br /> Local Creators.
+          <br /> Local Creators
         </h2>
+        <p className={styles.copy_sub}>
+          Find the perfect creative in Toronto, Canada for your project
+        </p>
       </div>
-      <div>
+      <div className={styles.heading_sub}>
         <div> See who is hiring or creating in, Toronto, Canada</div>
         <div>Show creators in your area</div>
       </div>
-      <div></div>
+      <div>
+        <p>Ready to get started?</p>
+        <button>Sign Up</button>
+      </div>
     </main>
   );
 }

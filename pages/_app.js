c16a68e2e3c5250fba749a4154a2a27859ef4d71
Alex <alex.gillespie@live.com>
2022-07-05 17:19:17 -0400

    navbar debug

--- pages/_app.js
+++ pages/_app.js
@@ -3,36 +3,19 @@ import { AuthProvider } from '../firebase/context';
 import Navbar from '../components/Navbar';
 import Head from 'next/head';
 
+import { useRouter } from 'next/router';
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  console.log('router', router);
+
   return (
     <AuthProvider>
       <Head>
         <title>Content Connector</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Montserrat:wght@200;400&family=Roboto&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        /> */}
       </Head>
-      {/* <Navbar /> */}
+      {router.pathname !== '/' && <Navbar />}
       <Component {...pageProps} />
     </AuthProvider>
   );

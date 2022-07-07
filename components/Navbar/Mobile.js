f0b2609a54869b6e916bff9e9c51a58b6f42a591
Alex <alex.gillespie@live.com>
2022-07-07 17:24:13 -0400

    hamburger icon added

--- components/Navbar/Mobile.js
+++ components/Navbar/Mobile.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import styles from '../../styles/navbar.module.scss';
 
+import MenuIcon from '../../public/hamburger-menu.svg';
+import Image from 'next/image';
+
 const Mobile = () => {
-  return <div className={styles.mobile_container}>Mobile</div>;
+  return (
+    <div className={styles.mobile_container}>
+      <Image className={styles.menu_icon} src={MenuIcon} />
+    </div>
+  );
 };
 
 export default Mobile;

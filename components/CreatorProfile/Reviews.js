539e431fade85f2820fd71102d804ba0b76dc4d2
Alex <alex.gillespie@live.com>
2022-07-22 14:41:37 -0400

    basic reviews skeleton

--- components/CreatorProfile/Reviews.js
+++ components/CreatorProfile/Reviews.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import styles from '../../styles/creator_profile.module.scss';
 
-const Reviews = () => {
-  return <div>Reviews</div>;
+const reviews = [
+  {
+    overview: 'Outdoor family photo retouching and editing',
+    rating: 5,
+    date: 'Aug 8, 2019',
+    feedback:
+      'Mike was a pleasure to work with and adhered to all the stipulations we requested for this project. Would definitely work with him again.',
+  },
+  {
+    overview: 'Outdoor family photo retouching and editing',
+    rating: 5,
+    date: 'Aug 8, 2019',
+    feedback:
+      'Mike was a pleasure to work with and adhered to all the stipulations we requested for this project. Would definitely work with him again.',
+  },
+  {
+    overview: 'Outdoor family photo retouching and editing',
+    rating: 5,
+    date: 'Aug 8, 2019',
+    feedback:
+      'Mike was a pleasure to work with and adhered to all the stipulations we requested for this project. Would definitely work with him again.',
+  },
+  {
+    overview: 'Outdoor family photo retouching and editing',
+    rating: 5,
+    date: 'Aug 8, 2019',
+    feedback:
+      'Mike was a pleasure to work with and adhered to all the stipulations we requested for this project. Would definitely work with him again.',
+  },
+];
+
+const Reviews = ({}) => {
+  return (
+    <div className={styles.interface_reviews}>
+      {reviews.map(({ overview, rating, date, feedback }) => (
+        <div>
+          <h5>{overview}</h5>
+          <div>
+            <p>{rating}</p>
+            <p>{date}</p>
+          </div>
+          <div>{feedback}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Reviews;
--- components/CreatorProfile/index.js
+++ components/CreatorProfile/index.js
@@ -9,7 +9,7 @@ import Reviews from './Reviews';
 const index = ({ profile }) => {
   const { userData, currentUser } = useContext(AuthContext);
 
-  const [provileNav, setprovileNav] = useState('portfolio');
+  const [provileNav, setprovileNav] = useState('reviews');
   const { avatar, city, description, username, uid } = profile;
 
   return (

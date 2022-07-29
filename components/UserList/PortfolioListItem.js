d6c655ded9e430414d2d235481b69837de1fa6f0
Alex <alex.gillespie@live.com>
2022-07-29 14:06:38 -0400

    created portfolio list item component

---
+++ components/UserList/PortfolioListItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PortfolioListItem = ({ portfolio }) => {
+  return <div>PortfolioListItem</div>;
+};
+
+export default PortfolioListItem;
--- components/UserList/index.js
+++ components/UserList/index.js
@@ -4,19 +4,23 @@ import UserListItem from './UserListItem';
 import styles from '../../styles/user_list.module.scss';
 const UserList = ({ users, portfolios }) => {
   const { userData, currentUser } = useContext(AuthContext);
-
+  console.log(portfolios);
   return (
     <div className={styles.user_list_container}>
-      {users.map((user) => (
-        <UserListItem
-          key={user.uid}
-          username={user.username}
-          uid={user.uid}
-          avatar={user.avatar}
-          user={user}
-          city={user.city}
-        ></UserListItem>
-      ))}
+      {!portfolios.length &&
+        users.map((user) => (
+          <UserListItem
+            key={user.uid}
+            username={user.username}
+            uid={user.uid}
+            avatar={user.avatar}
+            user={user}
+            city={user.city}
+          ></UserListItem>
+        ))}
+
+      {portfolios.length &&
+        portfolios.map((portfolio) => <PortfolioListItem />)}
     </div>
   );
 };

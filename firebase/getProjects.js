2b5f22d9aeb5ce77e70514a4b010e9da257ad66f
Alex <alex.gillespie@live.com>
2022-08-09 22:15:40 -0400

    getProjects with tag

--- firebase/getProjects.js
+++ firebase/getProjects.js
@@ -1,15 +1,27 @@
 import { getDocs, collection, where } from 'firebase/firestore';
 import { firestore } from './clientApp';
 
-const getProjects = async (city, setProjects) => {
-  const querySnapshot = await getDocs(
-    collection(firestore, 'projects'),
-    where('city', '==', city)
-  );
+const getProjects = async (city, setProjects, tag = 0) => {
   const projects = [];
-  querySnapshot.forEach((doc) => {
-    projects.push(doc.data());
-  });
+  if (!tag) {
+    const querySnapshot = await getDocs(
+      collection(firestore, 'projects'),
+      where('city', '==', city)
+    );
+    querySnapshot.forEach((doc) => {
+      projects.push(doc.data());
+    });
+  } else {
+    const querySnapshot = await getDocs(
+      collection(firestore, 'projects'),
+      where('city', '==', city),
+      where('tags', 'array-contains', tag)
+    );
+    querySnapshot.forEach((doc) => {
+      projects.push(doc.data());
+    });
+  }
+
   setProjects(projects);
 };
 

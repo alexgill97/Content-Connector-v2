cc1e225fa617977860aefb49b5641cda4463a155
Alex <alex.gillespie@live.com>
2022-08-11 09:34:48 -0400

    EditProfile logic

--- components/Modal/EditProfile.js
+++ components/Modal/EditProfile.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import styles from '../../styles/modal.module.scss';
 
 const EditProfile = () => {
-  return <div>EditProfile</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      {!open && (
+        <div onClick={() => setOpen(true)}>
+          <button>Edit Profile</button>
+        </div>
+      )}
+      {open && (
+        <div>
+          <div className={styles.overlay}></div>
+          <div className={styles.modal_container}>
+            <form action="">Test</form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
-
 export default EditProfile;

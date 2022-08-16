bb47bc2a835fa84a679fc075dc9335d9984bbbac
Alex <alex.gillespie@live.com>
2022-08-16 15:06:38 -0400

    added services completed total in profile details

--- components/CreatorProfile/ProfileDetails.js
+++ components/CreatorProfile/ProfileDetails.js
@@ -4,18 +4,24 @@ import styles from '../../styles/creator_profile.module.scss';
 const ProfileDetails = ({ userData }) => {
   return (
     <div className={styles.profiledetails}>
-      <div className={styles.profiledetails_single}>
+      <div className={styles.profiledetails__single}>
         <p>Avg. Response Time</p>
         <p>1 hour</p>
       </div>
-      <div className={styles.profiledetails_single}>
+      <div className={styles.profiledetails__single}>
         <p>Total Successful Projects</p>
         <p>{userData.completedProjects || 0}</p>
       </div>
-      <div className={styles.profiledetails_list}>
-        <h5>Services</h5>
+      <div className={styles.profiledetails__list}>
+        <div className={styles.profiledetails__services_header}>
+          <h5>Services</h5>
+          <p># completed</p>
+        </div>
         {userData.services?.map((service) => (
-          <p key={service}>{service}</p>
+          <div className={styles.profiledetails__services_list}>
+            <p key={service}>{service}</p>
+            <p> 4</p>
+          </div>
         ))}
       </div>
       <div className={styles.profiledetails_list}>

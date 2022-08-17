8431da6de9754d7a54e32b0a5a6c3d0ccba5e43e
Alex <alex.gillespie@live.com>
2022-08-17 16:33:05 -0400

    type filter modal

--- components/FindProjects/Filter/TypeFilter.js
+++ components/FindProjects/Filter/TypeFilter.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../../styles/find_projects.module.scss';
 
 const TypeFilter = () => {
-  return <div className={styles.filter__modal}>TypeFilter</div>;
+  const [mediaType, setMediaType] = useState('both');
+
+  return (
+    <div className={styles.filter__modal}>
+      <button
+        onClick={() => setMediaType('both')}
+        className={mediaType === 'both' && styles.type__selected}
+      >
+        Both
+      </button>
+      <button
+        onClick={() => setMediaType('photography')}
+        className={mediaType === 'photography' ? styles.type__selected : null}
+      >
+        Photography
+      </button>
+      <button
+        onClick={() => setMediaType('videography')}
+        className={mediaType === 'videography' ? styles.type__selected : null}
+      >
+        Videography
+      </button>
+    </div>
+  );
 };
 
 export default TypeFilter;

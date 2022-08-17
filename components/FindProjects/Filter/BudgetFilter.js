e40179ed6e9c0e6860b72d474be6c06f350d274f
Alex <alex.gillespie@live.com>
2022-08-17 15:56:48 -0400

    BudgetFilter modal logic

--- components/FindProjects/Filter/BudgetFilter.js
+++ components/FindProjects/Filter/BudgetFilter.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../../styles/find_projects.module.scss';
 
 const BudgetFilter = () => {
-  return <div className={styles.filter__modal}>BudgetFilter</div>;
+  const [minimum, setMinimum] = useState(0);
+  const [maximum, setMaximum] = useState(0);
+
+  return (
+    <div className={styles.filter__modal}>
+      <div className={styles.budget__input_container}>
+        <input
+          type="tel"
+          value={minimum}
+          onChange={(e) => setMinimum(e.target.value)}
+        />
+        <p>-</p>
+        <input
+          type="tel"
+          value={maximum}
+          onChange={(e) => setMaximum(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default BudgetFilter;

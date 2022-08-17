4fef688e9f7609dbd15bc2111cccba17d409eccf
Alex <alex.gillespie@live.com>
2022-08-17 13:14:53 -0400

    handle state changes within category component

--- components/FindProjects/Filter/CategoryFilter.js
+++ components/FindProjects/Filter/CategoryFilter.js
@@ -1,34 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../../styles/find_projects.module.scss';
 
-const checkboxCategories = [
-  { displayName: 'Wedding', value: true },
-  { displayName: 'Lifestyle', value: true },
-  { displayName: 'Product', value: true },
-  { displayName: 'Fashion', value: true },
-  { displayName: 'Sports', value: true },
-  { displayName: 'Real Estate', value: true },
-  { displayName: 'Automotive', value: true },
-  { displayName: 'Portrait', value: true },
-  { displayName: 'Brand', value: true },
-  { displayName: 'Event', value: true },
-];
+// const checkboxCategories = [
+//   { displayName: 'Wedding', value: true },
+//   { displayName: 'Lifestyle', value: true },
+//   { displayName: 'Product', value: true },
+//   { displayName: 'Fashion', value: true },
+//   { displayName: 'Sports', value: true },
+//   { displayName: 'Real Estate', value: true },
+//   { displayName: 'Automotive', value: true },
+//   { displayName: 'Portrait', value: true },
+//   { displayName: 'Brand', value: true },
+//   { displayName: 'Event', value: true },
+// ];
 
 const CategoryFilter = () => {
-  return (
-    <div className={styles.filter__modal}>
-      <label htmlFor="wedding">
-        <input type="checkbox" name="wedding" id="wedding" value={wedding} />
-        Wedding
-      </label>
-      {/* <input type="checkbox" id="wedding" />
-      <label htmlFor="">Product</label>
-      <input type="checkbox" id="wedding" />
-      <label htmlFor="">Wedding</label>
-      <input type="checkbox" id="wedding" />
-      <label htmlFor="">Wedding</label> */}
+  const [checkboxCategories, setCheckboxCategory] = useState([
+    { displayName: 'Wedding', value: true },
+    { displayName: 'Lifestyle', value: true },
+    { displayName: 'Product', value: true },
+    { displayName: 'Fashion', value: true },
+    { displayName: 'Sports', value: true },
+    { displayName: 'Real Estate', value: true },
+    { displayName: 'Automotive', value: true },
+    { displayName: 'Portrait', value: true },
+    { displayName: 'Brand', value: true },
+    { displayName: 'Event', value: true },
+  ]);
+
+  const handleChange = (event, category, index) => {
+    let tempCheckboxCategories = [...checkboxCategories];
+    tempCheckboxCategories[index].value = !category.value;
+    setCheckboxCategory(tempCheckboxCategories);
+  };
+
+  const listCategories = checkboxCategories.map((category, index) => (
+    <div>
+      <input
+        type="checkbox"
+        name=""
+        value={category}
+        checked={category.value}
+        onChange={(e) => handleChange(e, category, index)}
+        id={category.displayName}
+      />
+      <label htmlFor={category.displayName}>{category.displayName}</label>
     </div>
-  );
+  ));
+
+  return <div className={styles.filter__modal}>{listCategories}</div>;
 };
 
 export default CategoryFilter;
